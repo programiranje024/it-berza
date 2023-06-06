055b1600d6798b261ad7cbc8f55c17c258300800
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-06 20:57:40 +0200

    feat: initial commit (db, docker, php)

---
+++ db/schema.sql
@@ -0,0 +1,56 @@
+DROP TABLE IF EXISTS jobs;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name TEXT NOT NULL,
+  phone TEXT NOT NULL,
+  email TEXT NOT NULL,
+  bio TEXT NOT NULL,
+  password TEXT NOT NULL,
+  role TEXT NOT NULL DEFAULT "user" CHECK(role IN("user", "company", "admin")),
+  banned BOOLEAN NOT NULL DEFAULT FALSE,
+  verified BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name TEXT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS jobs (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  category_id INT NOT NULL,
+  company_id INT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (category_id) REFERENCES categories(id),
+  FOREIGN KEY (company_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  sender_id INT NOT NULL,
+  receiver_id INT NOT NULL,
+  message TEXT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (sender_id) REFERENCES users(id),
+  FOREIGN KEY (receiver_id) REFERENCES users(id)
+);
+
+INSERT INTO categories (name) VALUES
+  ("Web Development"),
+  ("Mobile Development"),
+  ("Data Science"),
+  ("Machine Learning"),
+  ("Artificial Intelligence"),
+  ("Blockchain"),
+  ("UI/UX Design"),
+  ("DevOps"),
+  ("Quality Assurance"),
+  ("Other");
